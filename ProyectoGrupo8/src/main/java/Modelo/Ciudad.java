ff6255d860a97592478906608086ccca327e188a
Quizhpi-Beatriz <bquizhpi@espol.edu.ec>
2021-11-26 18:05:34 -0500

    Se añadió la clase Persona y la clase ciudad

---
+++ ProyectoGrupo8/src/main/java/Modelo/Ciudad.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author Beatriz Quizhpi
+ */
+public class Ciudad {
+    private String nombre; 
+    private String provincia; 
+
+    public Ciudad(String nombre){
+        this.nombre= nombre; 
+    }
+    public Ciudad(String nombre, String provincia) {
+        this.nombre = nombre;
+        this.provincia = provincia;
+    }
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getProvincia() {
+        return provincia;
+    }
+
+    public void setProvincia(String provincia) {
+        this.provincia = provincia;
+    }
+    
+    
+}
---
+++ ProyectoGrupo8/src/main/java/Modelo/Persona.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author Beatriz Quizhpi
+ */
+public class Persona {
+    private String nombre; 
+    private String direccion; 
+    private String telefono; 
+    private Ciudad ciudad; 
+    private String email; 
+
+    public Persona(String nombre, String direccion, String telefono, Ciudad ciudad, String email) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.ciudad = ciudad;
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" + "nombre=" + nombre + ", direccion=" + direccion + ", telefono=" + telefono + ", ciudad=" + ciudad + ", email=" + email + '}';
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public Ciudad getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(Ciudad ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    
+}
