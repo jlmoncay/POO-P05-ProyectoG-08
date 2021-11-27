06df646fef85b4f685ac6597bdbb787095c6d5c4
jlmoncay <jlmoncay@espol.edu.ec>
2021-11-26 20:47:35 -0500

    Se creó la clase Premio.

---
+++ ProyectoGrupo8/src/main/java/Modelo/Premio.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author Jorge Moncayo Paz
+ */
+public class Premio {
+
+    private Auspiciante auspiciante;
+    private String lugar;
+    private String descripcion;
+
+    public Premio(Auspiciante auspiciante, String lugar, String descripcion) {
+        this.auspiciante = auspiciante;
+        this.lugar = lugar;
+        this.descripcion = descripcion;
+    }
+
+    public Auspiciante getAuspiciante() {
+        return auspiciante;
+    }
+
+    public void setAuspiciante(Auspiciante auspiciante) {
+        this.auspiciante = auspiciante;
+    }
+
+    public String getLugar() {
+        return lugar;
+    }
+
+    public void setLugar(String lugar) {
+        this.lugar = lugar;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    @Override
+    public String toString() {
+        return "Auspiciante: " + auspiciante + " | " + "Lugar: " + lugar + " | " + "Descripción: " + descripcion;
+    }
+
+}
